79f4ab9e66e55526f744e95f42686de98bb71f87
lucianossouza <luciano.illustrador@hotmail.com>
2020-06-09 22:16:52 -0300

    adicionar scripts

---
+++ scripts/intro.js
@@ -0,0 +1,70 @@
+// coments
+// document.write("Hello")
+
+// variaveis, tipos de dados
+// var myvar
+// myvar = "Hello"
+// let myvar = "Hi"
+// const myconst = "He"
+// document.write(myvar)
+
+// string
+//"Isso é uma string"
+//'Isso também é uma string'
+//'Isso é uma string também'
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+
+// document.write(bFalse)
+
+// objeto possuem propriedades e funcionalidade
+//const pessoa = {
+//  altura: "1,80m",
+//  idade: 24,
+//  solteiro: true,
+//  correr(){
+//   document.write("run Forest")
+//    return " run Forest"
+//  }
+// }
+
+// document.write(pessoa.correr())
+
+// Array - Vetores é uma coleção de dados
+// const colecaoDeBolinhas = ["blue","green", 1, {name: "My name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funções - Funcionalidades - Atalhos - Reuso de código
+
+// registrar função
+// function sayMyName(name) {
+  
+//  document.write(name)}
+
+// executar
+// sayMyName("Luciano")
+
+// const notaFinal = 7
+
+// if( notaFinal < 5 ) {
+//  document.write("Reprovado")
+  // caminho 1
+// } else {
+  // caminho 2
+//  document.write("Aprovado")
+// }
+
+
+// Loop repetições
+
+//for (i = 0; i< < 10; i++) {
+//  docuent.write('<p>$(i)</p>')
+// }
